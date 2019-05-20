dddaeacc5f47fb1d3efa666a3c3de09ec6444e52
RedanHassoun <redan.has.1991@gmail.com>
2019-05-20 22:42:21 +0300

    s

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,18 +2,16 @@ package com.example.demo;
 
 import com.example.demo.models.ApplicationUser;
 import com.example.demo.repo.ApplicationUserRepository;
-import com.example.demo.services.UserDetailsServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import java.util.stream.Stream;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -23,7 +21,8 @@ public class DemoApplication {
 		return new BCryptPasswordEncoder();
 	}
 
-	@Bean CommandLineRunner init(ApplicationUserRepository userRepository) {
+	@Bean
+    CommandLineRunner init(ApplicationUserRepository userRepository) {
 		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
 		return args -> {
 			Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel")
