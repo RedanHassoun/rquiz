a54c9f7e46b1226df22e62874e5bd27836e47ded
RedanHassoun <redan.has.1991@gmail.com>
2019-07-06 01:05:00 +0300

    redirect to quiz list after login

--- angularclient/src/app/core/components/login/login.component.ts
+++ angularclient/src/app/core/components/login/login.component.ts
@@ -34,7 +34,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     if (this.authService.isLoggedIn()) {
-      this.router.navigate(['/users']);
+      this.router.navigate(['/quizList']);
     }
   }
 }
