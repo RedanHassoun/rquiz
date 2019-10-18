2f0d54f48ac592620c92ba135f066ff64a14c4f1
RedanHassoun <redan.has.1991@gmail.com>
2019-10-18 23:59:28 +0300

    added integration with postgres

--- backend/src/main/java/com/raiseup/rquiz/common/AppConstants.java
+++ backend/src/main/java/com/raiseup/rquiz/common/AppConstants.java
@@ -2,7 +2,7 @@ package com.raiseup.rquiz.common;
 
 public class AppConstants {
     public static class DBConsts {
-        public static final String USERS_TABLE_NAME = "user";
+        public static final String USERS_TABLE_NAME = "app_user";
         public static final String QUIZ_ANSWER_TABLE_NAME = "quiz_answer";
         public static final String QUIZ_TABLE_NAME = "quiz";
         public static final String USER_ANSWER_TABLE_NAME = "quiz_user_answer";
