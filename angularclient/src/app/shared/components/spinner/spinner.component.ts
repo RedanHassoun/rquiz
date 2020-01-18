4b4d811dec668fc7e95a93cf2d4d7f868486e15c
RedanHassoun <redan.has.1991@gmail.com>
2020-01-18 14:29:35 +0200

    improved the spinner component

--- angularclient/src/app/shared/components/spinner/spinner.component.ts
+++ angularclient/src/app/shared/components/spinner/spinner.component.ts
@@ -1,5 +1,6 @@
-import { Observable } from 'rxjs';
-import { Component, OnInit, Input } from '@angular/core';
+import { AppUtil } from './../../util/app-util';
+import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit, Input, EventEmitter, Output, OnDestroy } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { isLoading$ } from '../../decorators/spinner-decorators';
 
@@ -8,20 +9,34 @@ import { isLoading$ } from '../../decorators/spinner-decorators';
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss']
 })
-export class SpinnerComponent implements OnInit {
+export class SpinnerComponent implements OnInit, OnDestroy {
   @Input() loadingText = 'Please wait...';
+  @Input() showCancel = false;
+  @Output() cancelButtonClicked = new EventEmitter();
+
+  private subscriptions: Subscription[] = [];
 
   constructor(private spinner: NgxSpinnerService) { }
 
   ngOnInit() {
-    isLoading$.subscribe((isLoading: boolean) => {
-      if (!!isLoading) {
-        this.spinner.show();
-      } else {
+    this.subscriptions.push(
+      isLoading$.subscribe((isLoading: boolean) => {
+        if (!!isLoading) {
+          this.spinner.show();
+        } else {
+          this.spinner.hide();
+        }
+      }, err => {
         this.spinner.hide();
-      }
-    }, err => {
-      this.spinner.hide();
-    });
+      })
+    );
+  }
+
+  cancelClicked(): void {
+    this.cancelButtonClicked.emit();
+  }
+
+  ngOnDestroy(): void {
+    AppUtil.releaseSubscriptions(this.subscriptions);
   }
 }
